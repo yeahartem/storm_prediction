8a13a7503fbf941955fb850ce116fe6dda664d8e
teshbek <teshbek@gmail.com>
2023-05-20 01:52:58 +0300

    slurm update

--- slurm_run.sh
+++ slurm_run.sh
@@ -11,4 +11,4 @@ export WANDB_DATA_DIR=/app/wind/outputs
 export WANDB_DIR=/app/wind/outputs
 export WANDB_CACHE_DIR=/app/wind/outputs
 
-singularity run --bind $(pwd)/Wind:/app/wind --bind $(pwd)/data:/app/wind/data_mounted --pwd "/app/wind" docker://teshbek/xtr_weather:wind_dev116 pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116 && pip install -r $(pwd)/Wind/environments/pytorch1.13.1_cuda116/requirements_nv.txt && python3 Wind/src/regression/train.py
+singularity run --bind $(pwd)/Wind:/app/wind --bind $(pwd)/data:/app/wind/data_mounted --pwd "/app/wind" docker://teshbek/xtr_weather:wind_dev116 pip3 install --upgrade pip && pip3 install torch==1.13.1+cu116 torchvision==0.14.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116 && pip3 install -r $(pwd)/Wind/environments/pytorch1.13.1_cuda116/requirements_nv.txt && python3 Wind/src/regression/train.py
