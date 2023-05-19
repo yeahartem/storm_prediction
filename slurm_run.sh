f2de7ec7986292cd6f8e0273c6aab11f43f6bd10
teshbek <teshbek@gmail.com>
2023-05-20 00:50:08 +0300

    slurm update

--- slurm_run.sh
+++ slurm_run.sh
@@ -11,4 +11,4 @@ export WANDB_DATA_DIR=/app/wind/outputs
 export WANDB_DIR=/app/wind/outputs
 export WANDB_CACHE_DIR=/app/wind/outputs
 
-singularity run --bind -v $(pwd)/docker_repos/Wind:/app/wind --bind $(pwd)/docker_repos/Wind:/app/wind --pwd "/app/wind" xtr_weather_wind_dev116.sif python3 /app/wind/src/regression/train.py 
+singularity run --bind $(pwd)/Wind:/app/wind --bind $(pwd)/data:/app/wind/data_mounted --pwd "/app/wind" xtr_weather_wind_dev116.sif python3 /app/wind/src/regression/train.py 
