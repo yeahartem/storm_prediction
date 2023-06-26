a2e1d6a8418cf247301c778c8cd5245f1422e4a2
teshbek <teshbek@gmail.com>
2023-06-27 01:07:22 +0300

    test config added

---
+++ src/regression/test.py
@@ -0,0 +1,68 @@
+import sys,os
+sys.path.append(os.getcwd())
+import warnings
+import torch
+import random
+import logging
+import pytorch_lightning as pl
+from src.regression.models.pl_module import WindNetPL
+from datamodule import WindDataModule
+from datetime import datetime
+import hydra
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning.loggers import WandbLogger
+import wandb
+import time
+from pytorch_lightning.callbacks import LearningRateMonitor, OnExceptionCheckpoint
+
+warnings.filterwarnings("ignore")
+torch.manual_seed(112)
+random.seed(112)
+os.environ['WANDB_MODE'] = 'online'
+os.environ['WANDB_DIR'] = 'outputs/wandb'
+os.environ['WANDB_CONFIG_DIR'] = 'outputs/wandb'
+os.environ['WANDB_CACHE_DIR'] = 'outputs/wandb'
+logging.basicConfig(level=logging.INFO, format='%(asctime)s-%(message)s')
+torch.set_float32_matmul_precision('high')
+
+
+def test(cfg: DictConfig) -> None:        
+    start_time = time.process_time()  
+    wandb_logger = WandbLogger(save_dir=os.path.join(os.getcwd(), "outputs/wandb"),
+                               project=cfg.project_name,
+                               name=cfg.experiment_name)
+    dm = WindDataModule(cfg)
+    model = WindNetPL.load_from_checkpoint(os.path.join(os.getcwd(), "outputs", cfg.path_to_checkpoint), cfg=cfg)
+
+
+    # if torch.__version__ >= "2.0.0":
+    #     model = torch.compile(model)
+    # else:
+    #     print("PyTorch version is smaller than 2.0, compilation is not supported")
+
+    wandb_logger.watch(model, log='all', log_freq=100)       
+    lr_monitor = LearningRateMonitor(logging_interval='step', log_momentum=True)
+    default_root_dir = os.path.join(os.getcwd(), "outputs")#os.path.join(os.getcwd(), "outputs")
+    trainer = pl.Trainer(max_epochs=cfg.max_epoch,
+                         accelerator="gpu",
+                         precision=cfg.precision,
+                         benchmark=True,
+                         devices=[0],
+                         check_val_every_n_epoch=1,
+                         default_root_dir=default_root_dir,
+                         logger=wandb_logger,
+                         callbacks=[lr_monitor],) 
+
+    logging.info(f"Time to start test {time.process_time() - start_time} seconds")
+    trainer.test(model, dm)
+
+
+@hydra.main(version_base=None, config_path=os.path.join(os.getcwd(),"configs/test_configs"), config_name="test_world_reg_test")
+def main(cfg: DictConfig):    
+    test(cfg)
+    logging.info('Test finished!')
+
+
+if __name__ == "__main__":      
+    main()
+    wandb.finish()
