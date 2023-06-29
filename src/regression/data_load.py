8ab409ee4f8e71cb7942eb3035ca61e764e10542
S.Lukashevich <vjugor1@gmail.com>
2023-06-29 18:06:37 +0300

    data inference preloader fix and clean

--- src/regression/data_load.py
+++ src/regression/data_load.py
@@ -178,6 +178,7 @@ class DataInferPreLoader(DataPreLoader):
         self.lat_idxs = np.where(np.logical_and((self.lat_coords <= lat_max), (self.lat_coords >= lat_min)))
         self.lon_coords = np.load(os.path.join(cfg.data_dir, 'lon.npy'))
         self.lon_idxs = np.where(np.logical_and((self.lon_coords <= lon_max), (self.lon_coords >= lon_min)))
+        self.time_idxs = self.align_time(self.time_coords)
         
         if self.data_exists():
             self.load_data()
@@ -185,10 +186,10 @@ class DataInferPreLoader(DataPreLoader):
         self.dataset_as_blocks = self.load_climate_data()
         # lat_idxs = np.arange(self.dataset_as_blocks.shape[0])
         # lon_idxs = np.arange(self.dataset_as_blocks.shape[1])
-        tim_idxs = np.arange(self.dataset_as_blocks.shape[2])
-        idxs_mesh = np.meshgrid(self.lat_idxs, self.lon_idxs, tim_idxs)
+        time_idxs = np.arange(self.dataset_as_blocks.shape[2])
+        idxs_mesh = np.meshgrid(self.lat_idxs, self.lon_idxs, time_idxs)
         self.train_data_idxs = None
-        self.test_data_idxs = np.vstack((idxs_mesh[0].flatten(), idxs_mesh[1].flatten(), idxs_mesh[2].flatten(), np.empty(len(idxs_mesh[2].flatten())))) # walk order: time -> lat -> lon (2 -> 0 -> 1)
+        self.test_data_idxs = np.vstack((idxs_mesh[0].flatten(), idxs_mesh[1].flatten(), idxs_mesh[2].flatten(), np.empty(len(idxs_mesh[2].flatten())))).astype(int) # walk order: time -> lat -> lon (2 -> 0 -> 1)
         # self.test_data_idxs = np.vstack((self.test_data_idxs, (-1) * np.ones(self.test_data_idxs[-1], dtype=int)))
         # self.train_data_idxs = np.concatenate(train_data_idxs, axis=1)
         # self.test_data_idxs = np.concatenate(test_data_idxs, axis=1)
