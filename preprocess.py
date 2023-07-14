fa25f32418807cf10b337846ec67dae4d84624de
teshbek <teshbek@gmail.com>
2023-07-14 18:04:21 +0300

    config fix

--- preprocess.py
+++ preprocess.py
@@ -8,7 +8,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf, ListConfig
 from src.data_assemble.prepare_cmip5 import prepare_cmip
 
-@hydra.main(version_base=None, config_path=os.path.join(os.getcwd(),"configs/dataset_configs"), config_name="cmip6_dataset_world_test.yaml")
+@hydra.main(version_base=None, config_path=os.path.join(os.getcwd(),"configs/infer_configs"), config_name="cmip5_w_eval.yaml")
 def run_prepare(cfg: DictConfig):
     prepare_cmip(cfg)
 
--- run.py
+++ run.py
@@ -40,7 +40,7 @@ supported_drivers['LIBKML'] = 'rw'
 logging.basicConfig(level=logging.INFO, format='%(asctime)s-%(message)s')
 
 
-@hydra.main(version_base=None, config_path=os.path.join(os.getcwd(),"configs/infer_configs"), config_name="infer_reg_test.yaml")
+@hydra.main(version_base=None, config_path=os.path.join(os.getcwd(),"configs/infer_configs"), config_name="cmip5_w_eval.yaml")
 def main(cfg: DictConfig):    
     eval(cfg)
     risk_estimation(cfg)
