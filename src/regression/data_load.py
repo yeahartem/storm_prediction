3bb27088de0f04eb1f7b3a9912ec6d7533057156
S.Lukashevich <vjugor1@gmail.com>
2023-07-25 15:29:05 +0300

    map pad, train ok, erased filter stations

--- src/regression/data_load.py
+++ src/regression/data_load.py
@@ -24,12 +24,13 @@ class DataPreLoaderAlt:
         self.time_coords = np.load(os.path.join(cfg.data_dir, 'time.npy')).astype('datetime64[D]')
         self.lat_coords = np.load(os.path.join(cfg.data_dir, 'lat.npy'))
         self.lon_coords = np.load(os.path.join(cfg.data_dir, 'lon.npy'))
+        self.dataset_torch = self.load_climate_data()
         if self.data_exists():
             self.load_data()
         else:
             self.prepare_target()
             self.save_data()
-        self.dataset_torch = self.load_climate_data()
+        
 
         if self.cfg.normalize:
             mean_channels = np.load(os.path.join(self.cfg.data_dir, f"mean_{cfg.precision}.npy"))
@@ -52,7 +53,10 @@ class DataPreLoaderAlt:
         for i, var in enumerate(self.cfg.variables):
             var_data[i] = np.load(os.path.join(self.cfg.data_dir, var + f'_{self.cfg.precision}.npy'))
 
-        var_data_torch = torch.from_numpy(var_data).half() if self.cfg.precision == 16 else torch.from_numpy(var_data)
+        #map padding
+        var_data_padded = self.make_padding(var_data)
+
+        var_data_torch = torch.from_numpy(var_data_padded).half() if self.cfg.precision == 16 else torch.from_numpy(var_data_padded)
         logging.info(f"Climate data preparation took {time.process_time() - start_time} seconds")
         return var_data_torch
 
@@ -84,7 +88,34 @@ class DataPreLoaderAlt:
                             )
         logging.info(f"Closest pixel search took {time.process_time() - start_time} seconds")
         return stations_df
+
+    def make_padding(self, data):
+        quadrants = self.extract_quadrants(data)
+        padded_map = self.assemble_padded_map(quadrants)
+        return padded_map
     
+    def extract_quadrants(self, data):
+        halfs = {"lat": data.shape[-2] // 2, "lon": data.shape[-1] // 2}
+        first_quadrant  = data[..., halfs['lat']:, halfs['lon']:]
+        second_quadrant = data[..., halfs['lat']:, :halfs['lon']]
+        third_quadrant  = data[..., :halfs['lat'], :halfs['lon']]
+        fourth_quadrant = data[..., :halfs['lat'], halfs['lon']:]
+        self.fourth_q_shape = (fourth_quadrant.shape[-2], fourth_quadrant.shape[-1])
+        return first_quadrant, second_quadrant, third_quadrant, fourth_quadrant
+
+    def assemble_padded_map(self, quadrants):
+        try:
+            q_flipped = [q.reindex(lat=list(reversed(q.lat))) for q in quadrants]
+        except AttributeError:
+            q_flipped = [np.flip(q, axis=-2) for q in quadrants]
+        
+        column_0 = np.concatenate([q_flipped[2], quadrants[3], quadrants[0], q_flipped[1]], axis=-2)
+        column_1 = np.concatenate([q_flipped[3], quadrants[2], quadrants[1], q_flipped[0]], axis=-2)
+        #0 1 0 1
+
+        padded_map = np.concatenate((column_0, column_1, column_0, column_1), axis=-1)
+
+        return padded_map
 
     @staticmethod
     def round_to_closest_indices(arr, values):
@@ -131,11 +162,11 @@ class DataPreLoaderAlt:
         target_df = self.stations_to_data_grid(stations_df=target_df)
         target_df = target_df.drop("station_name")
         # filter out stations that are too close to the edge
-        target_df = target_df.filter((polars.col("lat") > self.cfg.half_side_size) &
-                                     (polars.col("lon") > self.cfg.half_side_size) &
-                                     (polars.col("lat") < (len(self.lat_coords) - self.cfg.half_side_size - 1)) &
-                                     (polars.col("lon") < (len(self.lon_coords) - self.cfg.half_side_size- 1))
-                                    )
+        # target_df = target_df.filter((polars.col("lat") > self.cfg.half_side_size) &
+        #                              (polars.col("lon") > self.cfg.half_side_size) &
+        #                              (polars.col("lat") < (len(self.lat_coords) - self.cfg.half_side_size - 1)) &
+        #                              (polars.col("lon") < (len(self.lon_coords) - self.cfg.half_side_size- 1))
+        #                             )
         
         logging.info(f"Stations before droppping: {len(target_df)}")
         logging.info(f"Time to prepare target {time.process_time() - start_time} seconds")
@@ -167,6 +198,11 @@ class DataPreLoaderAlt:
         self.stations = np.array(stations)
         self.train_data_idxs = np.concatenate(train_data_idxs, axis=1)
         self.test_data_idxs = np.concatenate(test_data_idxs, axis=1)
+        #index shift due to padding
+        self.train_data_idxs[0, :] += self.fourth_q_shape[0] #lat
+        self.train_data_idxs[1, :] += self.fourth_q_shape[1] #lon
+        self.test_data_idxs[0, :] += self.fourth_q_shape[0]  #lat
+        self.test_data_idxs[1, :] += self.fourth_q_shape[1]  #lon
         logging.info(f'Records prepared train {self.train_data_idxs.shape[1]}')
         logging.info(f'Records prepared test {self.test_data_idxs.shape[1]}')
         gc.collect()
